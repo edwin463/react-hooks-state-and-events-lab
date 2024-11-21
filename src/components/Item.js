b4e6a139a616900569465eeeb6a62ab367851c1a
edwin karuri njoroge <edwin.njoroge1@student.moringaschool.com>
2024-11-21 17:17:05 +0300

    reactstate

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,11 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
 
 function Item({ name, category }) {
+  const [isInCart, setIsInCart] = useState(false);
+
+  // Toggle cart state
+  function handleAddToCartClick() {
+    setIsInCart((prevState) => !prevState);
+  }
+
+  // Determine the class for the <li>
+  const liClass = isInCart ? "in-cart" : "";
+
   return (
-    <li className="">
+    <li className={liClass}>
       <span>{name}</span>
       <span className="category">{category}</span>
-      <button className="add">Add to Cart</button>
+      <button 
+        className={isInCart ? "remove" : "add"} 
+        onClick={handleAddToCartClick}
+      >
+        {isInCart ? "Remove From Cart" : "Add to Cart"}
+      </button>
     </li>
   );
 }
